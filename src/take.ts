b169f42e6c22e29647c3768431fb4324920b1b43
luxurioussauce <lavishbehemoth@gmail.com>
2025-01-31 18:28:24 -0500

    Separating arbTakeLiquidation into its own function

--- src/take.ts
+++ src/take.ts
@@ -5,22 +5,15 @@ import { KeeperConfig, PoolConfig } from './config';
 import { getAuctionPrice } from './price';
 import { getTime } from './time';
 
-export async function handleArbTakes(handleArbParams: {
+interface HandleArbParams {
   signer: Signer,
   pool: FungiblePool,
   poolConfig: PoolConfig,
   config: Pick<KeeperConfig, "dryRun" | "subgraphUrl" | "delayBetweenActions">,
-}) {
-  const {
-    signer,
-    pool,
-    poolConfig,
-    config: {
-      subgraphUrl,
-      delayBetweenActions,
-      dryRun,
-    }
-  } = handleArbParams;
+}
+
+export async function handleArbTakes({signer, pool, poolConfig, config}: HandleArbParams) {
+  const { subgraphUrl, delayBetweenActions, dryRun, } = config;
   const {pool: {hpb, hpbIndex, liquidationAuctions}} = await getLiquidations(subgraphUrl, pool.poolAddress, poolConfig.take!.minCollateral);
   for (const auction of liquidationAuctions) {
     const {borrower, kickTime, referencePrice} = auction;
@@ -28,23 +21,44 @@ export async function handleArbTakes(handleArbParams: {
     const currentPrice = getAuctionPrice(referencePrice, timeElapsed);
 
     if (currentPrice < hpb) {
-      if (dryRun) {
-        console.log(`DryRun - would ArbTake - poolAddress: ${pool.poolAddress}, borrower: ${borrower}, currentPrice: ${currentPrice}, hpb: ${hpb}`);
-      } else {
-        // TODO: should we loop through this step until collateral remaining is zero?
-        console.log(`Sending ArbTake Tx - poolAddress: ${pool.poolAddress}, borrower: ${borrower}, currentPrice: ${currentPrice}, hpb: ${hpb}`);
-        const liquidationSdk = pool.getLiquidation(borrower);
-        const arbTakeTx = await liquidationSdk.arbTake(signer, hpbIndex);
-        await arbTakeTx.verifyAndSubmit();
-        console.log(`ArbTake successful - poolAddress: ${pool.poolAddress}, borrower: ${borrower}, currentPrice: ${currentPrice}, hpb: ${hpb}`);
+      const liquidation = {borrower, hpbIndex};
+      await arbTakeLiquidation({pool, poolConfig, signer, liquidation, config});
+    }
+  }
+}
 
-        // withdraw liquidity.
-        if (poolConfig.take!.withdrawRewardLiquidity) {
-          const withdrawTx = await pool.withdrawLiquidity(signer, [hpbIndex]);
-          await withdrawTx.verifyAndSubmit();
-          await delay(delayBetweenActions);
-        }
-      }
+interface LiquidationToArbTake {
+  borrower: string;
+  hpbIndex: number;
+}
+
+interface ArbTakeLiquidationParams {
+  pool: FungiblePool,
+  poolConfig: PoolConfig,
+  signer: Signer,
+  liquidation: LiquidationToArbTake,
+  config: Pick<KeeperConfig, "delayBetweenActions" | "dryRun">,
+}
+
+async function arbTakeLiquidation({pool, poolConfig, signer, liquidation, config}: ArbTakeLiquidationParams) {
+  const {borrower, hpbIndex} = liquidation;
+  const {delayBetweenActions, dryRun} = config;
+
+  if (dryRun) {
+    console.log(`DryRun - would ArbTake - poolAddress: ${pool.poolAddress}, borrower: ${borrower}`);
+  } else {
+    // TODO: should we loop through this step until collateral remaining is zero?
+    console.log(`Sending ArbTake Tx - poolAddress: ${pool.poolAddress}, borrower: ${borrower}`);
+    const liquidationSdk = pool.getLiquidation(borrower);
+    const arbTakeTx = await liquidationSdk.arbTake(signer, hpbIndex);
+    await arbTakeTx.verifyAndSubmit();
+    console.log(`ArbTake successful - poolAddress: ${pool.poolAddress}, borrower: ${borrower}`);
+
+    // withdraw liquidity.
+    if (poolConfig.take!.withdrawRewardLiquidity) {
+      const withdrawTx = await pool.withdrawLiquidity(signer, [hpbIndex]);
+      await withdrawTx.verifyAndSubmit();
+      await delay(delayBetweenActions);
     }
   }
 }
--- src/test/erc20.test.ts
+++ src/test/erc20.test.ts
@@ -1,9 +1,10 @@
 import { providers, Wallet, Contract, BigNumber, ContractTransaction } from 'ethers';
 import {Signer} from '@ajna-finance/sdk';
-import { approveErc20 } from '../erc20';
+import { approveErc20, getBalanceOfErc20 } from '../erc20';
 import { resetHardhat, getImpersonatedSigner, setBalance } from './test-utils';
 import {LOCAL_MAIN_NET_CONFIG} from './test-config';
 import {expect} from 'chai';
+import { wadToNumber } from '../utils';
 
 const WETH_WHALE_ADDRESS = '0xf04a5cc80b1e94c69b48f5ee68a08cd2f09a7c3e';
 const WETH_ABI = [
@@ -30,31 +31,24 @@ describe.only('approverErc20', () => {
   it('Can approve ERC20 transfer', async () => {
     const receiver = Wallet.fromMnemonic(LOCAL_MAIN_NET_CONFIG.USER1_MNEMONIC);
     const signer = await getImpersonatedSigner(WETH_WHALE_ADDRESS);
-    await setBalance(WETH_WHALE_ADDRESS, '0x10000000000000');
+    await setBalance(WETH_WHALE_ADDRESS, '0x100000000000000000');
     const approveTx = await approveErc20(signer, LOCAL_MAIN_NET_CONFIG.WSTETH_WETH_POOL.quoteTokenAddress, receiver.address, BigNumber.from("1000000000"));
-    console.log('is Instance of ContractTransaction:', Object.keys(approveTx));
     await approveTx.wait();
     const allowance = await getErc20Allowance(signer, LOCAL_MAIN_NET_CONFIG.WSTETH_WETH_POOL.quoteTokenAddress, WETH_WHALE_ADDRESS, receiver.address);
-    console.log("allowance:", allowance.toString())
-    expect(allowance.eq(BigNumber.from("1000000000"))).to.be.true;
+    expect(allowance.toString()).to.equal('1000000000');
   });
 });
 
-describe('getBallanceOfErc20', () => {
+describe.only('getBallanceOfErc20', () => {
 
   before(async () => {
     await resetHardhat();
   });
 
-  it('Can balance of ERC20', async () => {
-    const receiver = Wallet.fromMnemonic(LOCAL_MAIN_NET_CONFIG.USER1_MNEMONIC);
+  it('Can get balance of ERC20', async () => {
     const signer = await getImpersonatedSigner(WETH_WHALE_ADDRESS);
-    await setBalance(WETH_WHALE_ADDRESS, '0x10000000000000');
-    const approveTx = await approveErc20(signer, LOCAL_MAIN_NET_CONFIG.WSTETH_WETH_POOL.quoteTokenAddress, receiver.address, BigNumber.from("1000000000"));
-    await approveTx.wait();
-    const allowance = await getErc20Allowance(signer, LOCAL_MAIN_NET_CONFIG.WSTETH_WETH_POOL.quoteTokenAddress, WETH_WHALE_ADDRESS, receiver.address);
-    console.log("allowance:", allowance.toString())
-    expect(allowance.eq(BigNumber.from("1000000000"))).to.be.true;
-  })
-
+    const balanceBig = await getBalanceOfErc20(signer, LOCAL_MAIN_NET_CONFIG.WSTETH_WETH_POOL.quoteTokenAddress);
+    const balance = wadToNumber(balanceBig);
+    expect(balance).to.equal(604264.559344);
+  });
 });
